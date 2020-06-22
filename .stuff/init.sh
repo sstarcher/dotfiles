022e9133a5fd3ecaffdf3eb64f609daca3d32abc
Shane Starcher <shane.starcher@gmail.com>
2020-06-22 12:00:39 -0700

    updates for a fresh clone to use yadm and to run dotfiles

--- .stuff/init.sh
+++ .stuff/init.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew bundle 
+
+ssh-keygen -t rsa -b 4096 -C "shane.starcher@gmail.com"
+
+yadm clone git@github.com:sstarcher/dotfiles.git
 
 sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
 chsh -s /bin/zsh
@@ -11,9 +18,13 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # https://stackoverflow.com/questions/127591/using-caps-lock-as-esc-in-mac-os-x
 # https://github.com/unixorn/awesome-zsh-plugins
-# https://docs.docker.com/docker-for-mac/install/#download-docker-for-mac
 # Change iterm fonts, remember directory
 
+
+# Run mathiasbynes dotfiles
+bash <(curl -fsSL https://raw.githubusercontent.com/mathiasbynens/dotfiles/master/.macos)
+
+# Overrides for mathiasbynens's defaults
 defaults write -g InitialKeyRepeat -int 20
 defaults write -g KeyRepeat -int 3
 defaults write com.apple.finder AppleShowAllFiles YES;
@@ -24,3 +35,17 @@ defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm2"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
+# Dock Settings
+defaults write com.apple.dock tilesize -int 80
+
+###############################################################################
+# Kill affected applications                                                  #
+###############################################################################
+
+for app in "Activity Monitor" \
+	"Dock" \
+	; do
+	killall "${app}" &> /dev/null
+done
+
+echo 'To install fonts run `p10k configure`'
